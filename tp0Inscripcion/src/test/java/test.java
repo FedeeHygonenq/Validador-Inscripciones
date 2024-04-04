582052fd2425742da3ead1780f2211303d70f8e7
FHygonenq <federicohygonenq08@gmail.com>
2024-04-03 22:29:48 -0300

    Update test.java

--- tp0Inscripcion/src/test/java/test.java
+++ tp0Inscripcion/src/test/java/test.java
@@ -1,43 +1,44 @@
 package src.test.java;
 
-import org.junit.api.Assertions;
 import org.junit.Test;
+import src.main.java.Materia;
+import src.main.java.Inscripcion;
+import src.main.java.Alumno;
+import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-public class test {public class Test {
-
+public class InscripcionTest {
     @Test
-    public void alumnoPuedeInscribirseADiseño() {
-        Materia aYED = new Materia("AyED");
-        Materia discreta = new Materia("Discreta");
-        Materia ads = new Materia("Analisis De Sistemas", aYED, discreta);
-        Materia pdp = new Materia("Paradigmas de Programacion", aYED);
-        Materia dds = new Materia("Diseño de sistemas", ads, pdp);
+    public void testInscripcionAprobada() {
+        Alumno alumno = new Alumno(123);
+        Materia algoritmos = new Materia("Algoritmos y Estructuras de Datos");
+        Materia paradigmas = new Materia("Paradigmas de Programación");
+        Materia diseño = new Materia("Diseño de Sistemas");
 
-        Alumno pepito = new Alumno("Pepito", 2221, pdp, aYED);
-        Inscripcion inscripcion = new Inscripcion(pepito, dds);
-        Assertions.assertTrue(inscripcion.aprobada());
+        paradigmas.agregarCorrelativa(algoritmos);
+        diseño.agregarCorrelativa(paradigmas);
+
+
+        Inscripcion inscripcion = new Inscripcion(alumno);
+        inscripcion.agregarMateriaAInscribirse(diseño);
+
+        assertTrue(inscripcion.aprobada());
     }
 
     @Test
-    public void alumnoNoPuedeInscribirsePorqueNoAproboNada() {
-        Materia pdp = new Materia("Paradigmas de Programacion");
-        Materia aYED = new Materia("AyED");
-        Materia discreta = new Materia("Discreta");
+    public void testInscripcionNoAprobada() {
+        Alumno alumno = new Alumno(456);
+        Materia algoritmos = new Materia("Algoritmos y Estructuras de Datos");
+        Materia pdep = new Materia("Paradigmas de Programación");
+        Materia dds = new Materia("Diseño de Sistemas");
+        
+        pdep.agregarCorrelativa(algoritmos);
+        dds.agregarCorrelativa(pdep);
 
-        Alumno juan = new Alumno("Juan", 1);
-        Inscripcion inscripcion = new Inscripcion(juan, pdp, aYED, discreta);
-        Assertions.assertFalse(inscripcion.aprobada());
+
+        Inscripcion inscripcion = new Inscripcion(alumno);
+        inscripcion.agregarMateriaAInscribirse(dds);
+
+        assertFalse(inscripcion.aprobada());
     }
 
-    @Test
-    public void alumnoNoPuedeInscribirseAAnalisisDeSistemas() {
-        Materia aYED = new Materia("AyED");
-        Materia ads = new Materia("Analisis De Sistemas", aYED);
-
-        Alumno fede = new Alumno("Fede", 2, aYED);
-        Inscripcion inscripcion = new Inscripcion(fede, ads);
-        Assertions.assertFalse(inscripcion.aprobada());
-    }
-}
 }
